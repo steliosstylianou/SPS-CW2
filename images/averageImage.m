328a171753db8fecb0fe6b4fa7eefede4cb9fb1e
Stelios Stylianou <stylianoustelioscy@gmail.com>
2017-04-14 05:15:28 +0100

    Created functions for finding average fourier domain of each character.

---
+++ images/averageImage.m
@@ -0,0 +1,8 @@
+function meanf = averageImage(name)
+picture = fftv2([name,num2str(1),'.GIF']);
+sum = double(picture); % Inialize to first image.
+for i=2:10 % Read in remaining images.
+  value =fftv2([name,num2str(i),'.GIF']);
+  sum = sum + double(value);
+end;
+meanf = sum / 10;
--- images/fftv2.m
+++ images/fftv2.m
@@ -1,12 +1,14 @@
-f = imread('barbara.gif'); %read in image
+function f = fftv2(imagename)
+f = imread(imagename); %read in image
 z = fft2(double(f)); % do fourier transform
 q = fftshift(z); % puts u=0,v=0 in the centre
-Magq = abs(q); % magnitude spectrum
-Phaseq=angle(q); % phase spectrum
+f = (abs(q)); % magnitude spectrum
+%Phaseq=angle(q); % phase spectrum
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-% Usually for viewing purposes:
-imagesc(log(abs(q)+1));
-colorbar;
+% Usually for viewing purposes
+%imagesc(log(abs(q)+1));
+%figure;
+%imagesc(log(abs(q.^2)+1));
+%colorbar;
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-w = ifft2(ifftshift(q)); % do inverse fourier transform
-imagesc(w);
+end
