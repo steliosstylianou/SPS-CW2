73d67e190e84bab2748088eaa0d20b9b5f4f7b82
Stelios Stylianou <stylianoustelioscy@gmail.com>
2017-04-15 19:39:01 +0100

    Created decision boundaries

--- images/averageImage.m
+++ images/averageImage.m
@@ -4,6 +4,7 @@ sum = double(picture); % Inialize to first image.
 for i=2:10 % Read in remaining images.
   value =fftv2([name,num2str(i),'.GIF']);
   sum = sum + double(value);
-end;
+end
 meanf = sum / 10;
-meanf = log(meanf) + 1
+meanf = log(meanf) + 1;
+end
