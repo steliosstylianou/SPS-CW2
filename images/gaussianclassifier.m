a99058ef4ddb4c8412f4182825a3f0cb449a3146
Stelios Stylianou <stylianoustelioscy@gmail.com>
2017-04-17 18:17:40 +0100

    Init function

--- images/gaussianclassifier.m
+++ images/gaussianclassifier.m
@@ -1,13 +1,17 @@
 xa = trainfeat(1:10,:);
 xb = trainfeat(11:20,:);
 xc = trainfeat(21:30,:);
+
+hold on;
 scatter(xa(:, 1), xa(:,2), 'r', 'o');
 scatter(xb(:, 1), xb(:,2), 'b', 'x');
 scatter(xc(:, 1), xc(:,2), 'c', '*');
+
 % train the gaussians
 mua = mean(xa);
 mub = mean(xb);
 muc = mean(xc);
+
 xmu1 = mua(1);
 ymu1 = mua(2);
 xmu2 = mub(1);
@@ -15,19 +19,21 @@ ymu2 = mub(2);
 xmu3 = muc(1);
 ymu3 = muc(2);
 
-
-
 covara = cov(xa);
 covarb = cov(xb);
 covarc = cov(xc);
+
 testa = testfeat(1:5,:);
 testb = testfeat(6:10,:);
 testc = testfeat(11:15,:);
+
 testaOut = [mvnpdf(testfeat,mua,covara),mvnpdf(testfeat,mub,covarb),mvnpdf(testfeat,muc,covarc)];
-[maxaOut, classa] = max(testaOut,[],2);
+[maxaOut, class] = max(testaOut,[],2);
+
 xsd1 = std(xa(:,1));
 xsd2 = std(xb(:,1));
 xsd3 = std(xc(:,1));
+
 ysd1 = std(xa(:,2));
 ysd2 = std(xb(:,2));
 ysd3 = std(xc(:,2));
@@ -38,11 +44,12 @@ maxsd3 = max(xsd3,ysd3);
 
 x1 = xmu1-2*maxsd1:0.01:xmu1+2*maxsd1; % location of points at which x is calculated
 y1 = ymu1-2*maxsd1:0.01:ymu1+2*maxsd1; % location of points at which y is calculated
+
 [X1, Y1] = meshgrid(x1,y1); % matrices used for plotting
+
 %X1Y1 = reshape(X1Y1, size(mua));
 % Compute value of Gaussian pdf at each point in the grid
 z1 = mvnpdf(X1Y1, mua, covara);
 surf(x1,y1,z1);
 figure;
 contour(x1,y1,z1);
-kkkki
--- images/knn.m
+++ images/knn.m
@@ -4,26 +4,6 @@ for i = 1:10
     class(20+i,1) = 3;
 end
 
-for i = 1:10
-    pic = fftv2(['S',num2str(i),'.GIF']);
-    modpic = log(pic) + 1;
-    trainfeat(i,1) = ftriangle(modpic);
-    trainfeat(i,2) = frectangle(modpic);
-    
-    pic = fftv2(['T',num2str(i),'.GIF']);
-    modpic = log(pic) + 1;
-    trainfeat(10+i,1) = ftriangle(modpic);
-    trainfeat(10+i,2) = frectangle(modpic);
-
-    
-    pic = fftv2(['V',num2str(i),'.GIF']);
-    modpic = log(pic) + 1;
-    trainfeat(20+i,1) = ftriangle(modpic);
-    trainfeat(20+i,2) = frectangle(modpic);  
-
-end
-
-trainfeat = log(log(trainfeat));
 mdl = fitcknn(trainfeat, class, 'NumNeighbors', 3);
 
 %creating mesh range
@@ -46,24 +26,7 @@ set(gca,'ydir','normal');
 cmap = [1 0.8 0.8; 0.95 1 0.95; 0.9 0.9 1]
 colormap(cmap);
 hold on;
-for i=1:5
-    pic = fftv2(['S',num2str(10+i),'.gif']);
-    modpic = log(pic) + 1;
-    testfeat(i,1) = ftriangle(modpic);
-    testfeat(i,2) = frectangle(modpic);
-    
-    pic = fftv2(['T',num2str(10+i),'.gif']);
-    modpic = log(pic) + 1;
-    testfeat(5+i,1) = ftriangle(modpic);
-    testfeat(5+i,2) = frectangle(modpic);
 
-    pic = fftv2(['V',num2str(10+i),'.gif']);
-    modpic = log(pic) + 1;
-    testfeat(10+i,1) = ftriangle(modpic);
-    testfeat(10+i,2) = frectangle(modpic);    
-end
-    
-testfeat = log(log(testfeat));
 testlabel = predict(mdl,testfeat);
 trainlabel = predict(mdl,trainfeat);
 
@@ -75,19 +38,6 @@ scatter(trainfeat(1:10,1),trainfeat(1:10,2),'r','*');
 scatter(trainfeat(11:20,1),trainfeat(11:20,2),'g','*');
 scatter(trainfeat(21:30,1),trainfeat(21:30,2),'b','*');
 
-pic = fftv2('A1.GIF');
-modpic = log(pic) + 1;
-addfeat(1,1) = ftriangle(modpic);
-addfeat(1,2) = frectangle(modpic);
-
-pic = fftv2('B1.GIF');
-modpic = log(pic) + 1;
-addfeat(2,1) = ftriangle(modpic);
-addfeat(2,2) = frectangle(modpic);
-
 
 scatter(addfeat(1,1),addfeat(1,2),20,'c');
 scatter(addfeat(2,1),addfeat(2,2),20,'c');
-
-
-
