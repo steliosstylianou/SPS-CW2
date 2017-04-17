850fb7f39eaba26b802a21c076ef2afbd47452e4
Stelios Stylianou <stylianoustelioscy@gmail.com>
2017-04-17 19:58:54 +0100

    gaussian classifier

--- images/gaussianclassifier.m
+++ images/gaussianclassifier.m
@@ -19,15 +19,19 @@ ymu2 = mub(2);
 xmu3 = muc(1);
 ymu3 = muc(2);
 
-covara = cov(xa);
-covarb = cov(xb);
-covarc = cov(xc);
+cova = cov(xa);
+covb = cov(xb);
+covc = cov(xc);
+
+cova = eye(2);
+covb = eye(2);
+covc = eye(2);
 
 testa = testfeat(1:5,:);
 testb = testfeat(6:10,:);
 testc = testfeat(11:15,:);
 
-testaOut = [mvnpdf(testfeat,mua,covara),mvnpdf(testfeat,mub,covarb),mvnpdf(testfeat,muc,covarc)];
+testaOut = [mvnpdf(testfeat,mua,cova),mvnpdf(testfeat,mub,covb),mvnpdf(testfeat,muc,covc)];
 [maxaOut, class] = max(testaOut,[],2);
 
 xsd1 = std(xa(:,1));
@@ -42,27 +46,46 @@ maxsd1 = max(xsd1,ysd1);
 maxsd2 = max(xsd2,ysd2);
 maxsd3 = max(xsd3,ysd3);
 
-[x1,y1] = meshgrid(xmu1-2*maxsd1:0.01:xmu1+2*maxsd1,ymu1-2*maxsd1:0.01:ymu1+2*maxsd1);
-x1y1 = [x1(:) y1(:)];
-%X1Y1 = reshape(X1Y1, size(mua));
+x = linspace(2.27,2.37,1000);
+y = linspace(2.27,2.37,1000);
+
+[X,Y] = meshgrid(x,y);
+
+
+z1 = mvnpdf([X(:),Y(:)], mua, cova);
+z2 = mvnpdf([X(:),Y(:)], mub, covb);
+z3 = mvnpdf([X(:),Y(:)], muc, covc);
+
+z1 = reshape(z1, size(X));
+z2 = reshape(z2, size(X));
+z3 = reshape(z3, size(X));
 % Compute value of Gaussian pdf at each point in the grid
 
-[x2,y2] = meshgrid(xmu2-2*maxsd2:0.01:xmu2+2*maxsd2,ymu2-2*maxsd2:0.01:ymu2+2*maxsd2);
-x2y2 = [x2(:) y2(:)];
 
-[x3,y3] = meshgrid(xmu3-2*maxsd3:0.01:xmu3+2*maxsd3,ymu3-2*maxsd3:0.01:ymu3+2*maxsd3);
-x3y3 = [x3(:) y3(:)];
-
-z1 = mvnpdf(x1y1, mua, covara);
-z2 = mvnpdf(x2y2, mub, covarb);
-z3 = mvnpdf(x3y3, mub, covarb);
+E1 = 2*pi * sqrt(det(cova));
+P1 = (1/E1) * exp(-3);
+E2 = 2*pi * sqrt(det(covb));
+P2 = (1/E2) * exp(-3);
+E3 = 2*pi * sqrt(det(covc));
+P3 = (1/E3) * exp(-3);
 
 hold on;
 
-contour(x1,y1,z1);
-contour(x1,y1,z1);
-contour(x1,y1,z1);
+contour(X,Y,z1,[P1,P1]);
+contour(X,Y,z2,[P2,P2]);
+contour(X,Y,z3,[P3,P3]);
+
+hold off;
+
+ratioP1P2 = z1./z2;
+
+ratioP2P3 = z2./z3;
+
+ratioP1P3 = z3./z1;
 
 
-
+hold on;
+contour(X,Y,ratioP1P2,[1,1],'g');
+contour(X,Y,ratioP2P3,[1,1],'b');
+contour(X,Y,ratioP1P3,[1,1],'r');
 
